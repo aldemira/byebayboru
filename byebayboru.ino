4827630b9882fb34a70ceeb1f36d945e8a93a122
Aldemir Akpinar <aldemir.akpinar@gmail.com>
2016-03-18 10:58:50 +0200

    Added license

--- byebayboru.ino
+++ byebayboru.ino
@@ -1,3 +1,21 @@
+/*
+    Bye Bay Boru is an effort to create a robotic face
+    Copyright (C) 2016 Aldemir Akpinar
+    Copyright (C) 2016 Yüz Yüze Sanat (https://www.facebook.com/yuzyuzesanat)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "Thread.h"
 #include "ThreadController.h"
 #include <Servo.h>

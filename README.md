e1464b48dbfe98e8f58d9dcd4b4fc9a93abd2490
Aldemir Akpinar <aldemir.akpinar@gmail.com>
2016-05-05 17:32:40 +0300

    Now utilising GY-521 breakout board

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This project's aim is to create a robotic face using Arduino Mega. Arduino code is inspired by fritz project, however, there will not a PC to 
 manage or send messages to Arduino and Arduino Servo library will be used. 
 
-This program uses ArduinoThread library (https://github.com/ivanseidel/ArduinoThread)
+This program uses ArduinoThread (https://github.com/ivanseidel/ArduinoThread) and I2cdevlib (http://www.i2cdevlib.com/) libraries.
--- byebayboru.ino
+++ byebayboru.ino
@@ -19,9 +19,13 @@
 #include "Thread.h"
 #include "ThreadController.h"
 #include <Servo.h>
-#include <Wire.h>
-
-
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
 /****** PIN LAYOUT ************
   
   PIN 47  -> Eye Lid R
@@ -112,12 +116,34 @@ byte HEADLR_MAX_ANGLE = 90;
 byte mouth_ud_step = 5;
 
 /**** End Angle Definitions ********
-***** Accelerometer Thresholds ****/
-const int forwardThreshold = 340;
-const int backwardThreshold = 330;
-const int leftThreshold = 360;
-const int rightThreshold = 350;
-/**** END Accelerometer Thresholds ****/
+***** Accelerometer/Gyroscope Definitions ****/
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+/**** END Accelerometer/Gyroscope definitons ****/
 
 const int SILENT_AUDIO_VAL = 500;
 /****** End Configurable Stuff *********/
@@ -206,15 +232,6 @@ bool stopheart = false;
 bool iamcool = false;
 bool freeze = false;
 /**** End Face expression state variables ****/
-// Accelerometer Parameters
-//float xZero = 319;
-//float yZero = 337;
-//float zZero = 419;
-//int Scale = 1;
-double xAcc = 0;
-double yAcc = 0;
-double zAcc = 0;
-const float alpha = 1.5;
 
 // Make this false to stop random eyemovements
 bool enableeyemovements = true;
@@ -225,6 +242,66 @@ void setup()
 {
   // For debugging purposes
   Serial.begin(9600);
+  /************* Accelerometer / Gyroscope Initialization **************/  
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+// initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+/*
+    // wait for ready
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
+*/
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        //attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+  /********** End Accelerometer / Gyroscope Initialization ************/
+
   //SETUP heart
   pinMode(HEART_LED, OUTPUT);
   
@@ -349,8 +426,6 @@ void buttonHandler()
     // Serial.println("resetting face...");
     if(ignoreacc == true) {
       ignoreacc = false;
-      xAcc = 0;
-      zAcc = 0;
     }
     
     // Don't try to reset servos if we're trying to be cool
@@ -505,17 +580,59 @@ void moveHeadCallback()
   if(ignoreacc == true || ultimateaction == true)
     return;
   
-  int xAxis = analogRead(ACC_X_INPUT_PIN);
-  //int yAxis = analogRead(ACC_Y_INPUT_PIN);
-  int zAxis = analogRead(ACC_Z_INPUT_PIN);
-  //Low Pass Filter
-  xAcc = xAxis * alpha + (xAcc * (1.0 - alpha));
-  //yAcc = yAxis * alpha + (yAcc * (1.0 - alpha));
-  zAcc = zAxis * alpha + (zAcc * (1.0 - alpha));
-  double roll = (atan2(zAcc, xAcc)*180.0/PI);
-  //Serial.println(roll);
-  if(roll <= HEADLR_MAX_ANGLE && roll >= HEADLR_MIN_ANGLE)
-    headLR.write(roll+30);
+  // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    if (fifoCount < packetSize)
+      return;
+
+    // reset interrupt flag and get INT_STATUS byte
+    //mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            /* Head UP down
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            int tilt = ypr[1] * 180/M_PI;
+            Serial.print(tilt);
+            headUD.write(tilt+90);   // Rotation around Y
+            */
+            Serial.print("\t");
+            int roll = ypr[2]*180/M_PI;
+            Serial.println(roll);
+            if(roll <= HEADLR_MAX_ANGLE && roll >= HEADLR_MIN_ANGLE)
+              headLR.write(roll+30);   // Rotation around X
+        #endif
+
+    }
 }
 
 /*********** END CALLBACK FUNCTIONS *******************/
